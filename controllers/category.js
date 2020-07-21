6d245874e2e65bfe123d1ac0f5b42e33638625b7
phamkhachoa <phamkhachoabk@gmail.com>
2020-07-22 00:46:35 +0700

    hjh

--- controllers/category.js
+++ controllers/category.js
@@ -1,4 +1,7 @@
 const Category = require('../models/category');
+const formidable = require('formidable');
+const _ = require('lodash');
+const fs = require('fs');
 const {errorHandler} = require('../helpers/dbErrorHandler');
 
 exports.create = (req, res) => {
@@ -12,3 +15,64 @@ exports.create = (req, res) => {
         res.json({data});
     })
 };
+
+exports.categoryById = (req, res, next, id) => {
+    Category.findById(id).exec((err, category) => {
+        if (err || !category) {
+            return res.status(400).json({
+                error: 'Category not found'
+            })
+        }
+        return res.json(category);
+    })
+}
+
+exports.read = (req, res) => {
+    return res.json(req.category);
+}
+
+exports.remove = (req, res) => {
+    let category = req.category;
+    category.remove((err, deleteCategory) => {
+        if (err) {
+            return res.status(400).json({
+                error: errorHandler(err)
+            });
+        }
+        return res.json({
+            deleteCategory, message: 'Category deleted successfully'
+        })
+    })
+}
+
+exports.update = (req, res) => {
+    let form = new formidable.IncomingForm();
+    //form.keepExtensions = true;
+    form.parse(req, (err, fields) => {
+        if (err) {
+            return res.status(400).json({
+                error: 'Error when try to update category'
+            });
+        }
+
+        //check for all fields
+        const {name} = fields;
+        if (!name) {
+            return res.status(400).json({
+                error: 'All fields are required.'
+            })
+        }
+
+        let category = req.category;
+        category = _.extend(category, fields);
+
+        category.save((err, result) => {
+            if (err) {
+                return res.status(400).json({
+                    error: errorHandler(err)
+                })
+            }
+            res.json(result);
+        })
+    });
+}
--- routes/category.js
+++ routes/category.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const router = express.Router();
 
-const {create} = require('../controllers/category');
+const {create, categoryById, remove, update} = require('../controllers/category');
 const {requireSignin, isAuth, isAdmin} = require('../controllers/auth');
 const {userById} = require('../controllers/user');
 
 router.post('/category/create/:userId', requireSignin, isAuth, isAdmin, create);
+router.get('/category/:categoryId', categoryById);
+router.delete('/category/:categoryId/:userId', requireSignin, isAuth, isAdmin, remove);
+router.put('/category/:categoryId/:userId', requireSignin, isAuth, isAdmin, update);
 
+router.param('categoryId', categoryById);
 router.param('userId', userById);
 
 module.exports = router;
