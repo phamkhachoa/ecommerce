3543e897a878a3779367df8a67af93e100a4518b
hoapk <phamkhachoabk@gmail.com>
2020-07-21 13:59:26 +0700

    kdjf

--- controllers/product.js
+++ controllers/product.js
@@ -1,6 +1,7 @@
 const Product = require('../models/product');
 const formidable = require('formidable');
 const _ = require('lodash');
+const fs = require('fs');
 const {errorHandler} = require('../helpers/dbErrorHandler');
 
 exports.create = (req, res) => {
@@ -13,10 +14,69 @@ exports.create = (req, res) => {
             })
         }
 
-        let product = new Product(fields);
-        if (files.photo) {
-            product.photo.data = fs.readFileSync(files.photo.path);
-            product.photo.
+        //check for all fields
+        const {name, description, price, category, quantity, shipping} = fields;
+        if (!name || !description || !price || !category || !quantity || !shipping) {
+            return res.status(400).json({
+                error: 'All fields are required.'
+            })
         }
+
+        let product = new Product(fields);
+
+        // 1kb = 1000
+        // 1mb = 1000000
+        if (files.photo) {
+            //console.log(files.photo);
+            if (files.photo.size > 1000000) {
+                return res.status(400).json({
+                    error: 'Image should be less than 1mb in size.'
+                })
+            }
+            product.photo.data = fs.readFileSync(files.photo.path);
+            product.photo.contentType = files.photo.type;
+        }
+
+        product.save((err, result) => {
+            if (err) {
+                return res.status(400).json({
+                    error: errorHandler(err)
+                })
+            }
+            res.json(result);
+        })
     })
 };
+
+exports.productById = (req, res, next, id) => {
+    Product.findById(id).exec((err, product) =>{
+        if (err || !product) {
+            return res.status(400).json({
+                error: 'Product not found'
+            })
+        }
+
+        req.product = product;
+        next();
+    })
+};
+
+exports.read = (req, res) => {
+    req.product.photo = undefined;
+    return res.json(req.product);
+}
+
+exports.remove = (req, res) => {
+    let product = req.product;
+    product.remove((err, deleteProduct) => {
+        if (err) {
+            return res.status(400).json({
+                error: errorHandler(err)
+            });
+        }
+        deleteProduct.photo = undefined;
+        res.json({
+            deleteProduct, message: 'Product deleted successfully.'
+        })
+    })
+}
--- routes/product.js
+++ routes/product.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const router = express.Router();
 
-const {create} = require('../controllers/product');
+const {create, productById, read, remove} = require('../controllers/product');
 const {requireSignin, isAuth, isAdmin} = require('../controllers/auth');
 const {userById} = require('../controllers/user');
 
 router.post('/product/create/:userId', requireSignin, isAuth, isAdmin, create);
+router.get('/product/:productId', productById, read);
+router.delete('/product/:productId/:userId', requireSignin, isAuth, isAdmin, remove);
 
 router.param('userId', userById);
+router.param('productId', productById);
 
 module.exports = router;
